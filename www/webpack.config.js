20608e941d9726dadc239d4c8ae0e0ef3a216d02
ValentinGassend <valentin.gassend@gmail.com>
2022-02-11 14:10:31 +0100

    init webpack

---
+++ www/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+config = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+                test: /\.html/,
+                loader: 'html-loader',
+            },
+            { 
+                test: /\.hbs$/, 
+                loader: "handlebars-loader", 
+            }, {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                [
+                                    "postcss-preset-env",
+                                    {
+                                        // Options
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                }, 'sass-loader']
+            },
+            {
+                // Images
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+            {
+                // Fonts
+                test: /\.(woff(2)?|ttf|eot|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new ESLintPlugin({
+            context: path.resolve(__dirname, 'src'),
+            files: 'js/**/*.js',
+        })
+    ]
+};
+module.exports = config;
